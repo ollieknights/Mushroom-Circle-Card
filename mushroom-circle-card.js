12adf69b33d2842c73aab72e4049c3df82e2e902
ollieknights <69191206+ollieknights@users.noreply.github.com>
2024-11-30 18:28:14 +0000

    Delete mushroom-circle-card.js

--- mushroom-circle-card.js
+++
@@ -1,6 +0,0 @@
-const LitElement = window.LitElement || Object.getPrototypeOf(customElements.get("ha-panel-lovelace"));
-const html = window.lit.html;
-const css = window.lit.css;
-
-class MushroomCircleCard extends LitElement {
-  // ... rest of the code stays the same ...
